0baf850ca7648656e3a210e9861319bcd55b4c11
Adam Legg <adam.legg@gmail.com>
2014-06-19 14:35:11 -0600

    food

--- 06_favorite_food.rb
+++ 06_favorite_food.rb
@@ -1,3 +1,7 @@
 require_relative "person"
 
 # Print Bjorn's favorite foods. It should read "Bjorn's favorite foods are sushi, hamburgers, and mexican food."
+
+message = "Bjorn's favorite foods are #{BJORN_BORG["favorite_foods"].join(", ")}"
+
+puts message.insert(46, 'and ')
